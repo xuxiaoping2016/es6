79597166e065850d07ba869c787ec889dd3f8da8
xiaoping.xu <xiaoping.xu@weimob.com>
2019-07-09 19:17:43 +0800

    体积

---
+++ src/module/export/index.js
@@ -0,0 +1,5 @@
+import * as ht from './profile.js'
+
+// { firstName, lastName, year, multiply }
+
+console.log(`我的名字是${ht.firstName}${ht.lastName}，生于${ht.year}年，${ht.multiply(3,5)}`)
---
+++ src/module/export/profile.js
@@ -0,0 +1,8 @@
+export var firstName = 'Michael';
+export var lastName = 'Jackson';
+export var year = 1958;
+export function multiply(x, y) {
+    return x * y;
+  };
+
+// export { firstName, lastName, year, multiply }
